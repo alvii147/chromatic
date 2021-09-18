7cd4303eaa32262d362ca3a24693667a8e2a12c6
Zahin <alvii147@gmail.com>
2021-09-18 17:59:49 -0400

    updated dev.py and prod.py

--- chromatic/chromatic/settings/dev.py
+++ chromatic/chromatic/settings/dev.py
@@ -4,7 +4,3 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
--- chromatic/chromatic/settings/prod.py
+++ chromatic/chromatic/settings/prod.py
@@ -3,8 +3,4 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
-CORS_ALLOWED_ORIGINS = []
-
-CORS_ALLOWED_ORIGIN_REGEXES = []
+ALLOWED_HOSTS = ['devpostman404.pythonanywhere.com']
